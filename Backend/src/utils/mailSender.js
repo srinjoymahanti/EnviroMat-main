ab22a5404d4f341deaf2b796edd5b244918efeb9
Srinjoy <srinjoymahanti@gmail.com>
2025-12-31 13:41:47 +0530

    feat: Add mail sender utility using nodemailer for sending emails.

--- Backend/src/utils/mailSender.js
+++ Backend/src/utils/mailSender.js
@@ -4,13 +4,14 @@ const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: 2525, 
+      secure: false, // ✅ SSL connection
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
       // secure: false,
-      secure: true, // ✅ SSL connection
-       port: 465,     // ✅ SSL port for Gmail
+          // ✅ SSL port for Gmail
     })
 
     let info = await transporter.sendMail({
